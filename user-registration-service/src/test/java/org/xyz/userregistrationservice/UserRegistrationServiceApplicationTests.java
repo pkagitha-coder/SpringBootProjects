2920c459f26b2dbcc0d379661efe23a9cc6600b8
pkagitha <pkagitha@progress.com>
2020-02-20 16:31:38 +0530

    User registration service Spring Boot Project

---
+++ user-registration-service/src/test/java/org/xyz/userregistrationservice/UserRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xyz.userregistrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
